ca28c904c7c9d7172583be32f25b925e25a32702
someshyadav9404 <143208100+someshyadav9404@users.noreply.github.com>
2024-05-09 19:05:49 +0530

    Update views.py

--- Online_payment_fraud_detection/Online_payment_fraud_detection/views.py
+++ Online_payment_fraud_detection/Online_payment_fraud_detection/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render,redirect
 
 
 def homepage(request):
-    fns = 0
     try:
         txn = request.GET.get('transaction')
         amt = int(request.GET.get('amount'))
@@ -13,8 +12,6 @@ def homepage(request):
         ob = int(request.GET.get('Old Balance'))
         rnb = int(request.GET.get('Recipient New Balance'))
         rob = int(request.GET.get('Recipient Old Balance'))
-        print(amt - 18)
-        fns = amt - 18
     except:
         pass 
-    return render(request,"home.html",{'output':fns})
+    return render(request,"home.html")
