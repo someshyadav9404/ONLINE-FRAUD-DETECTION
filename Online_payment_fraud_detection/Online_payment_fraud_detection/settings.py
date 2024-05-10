a2553d29ee90ec3bda083d1d58df3be7101f362d
someshyadav9404 <143208100+someshyadav9404@users.noreply.github.com>
2024-05-10 07:57:56 +0000

    new code

--- Online_payment_fraud_detection/Online_payment_fraud_detection/settings.py
+++ Online_payment_fraud_detection/Online_payment_fraud_detection/settings.py
@@ -121,6 +121,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+LOGIN_REDIRECT_URL = "/"
 
 STATICFILES_DIRS =[
     BASE_DIR,"static"
--- Online_payment_fraud_detection/Online_payment_fraud_detection/urls.py
+++ Online_payment_fraud_detection/Online_payment_fraud_detection/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
 from Online_payment_fraud_detection import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('',views.homepage)
+    path("login/",include("django.contrib.auth.urls")),
+    path("",views.homepage)
 ]
