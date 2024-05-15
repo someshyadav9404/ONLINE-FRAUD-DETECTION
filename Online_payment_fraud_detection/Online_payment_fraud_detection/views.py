93694f5cc3f04e2bc930588b4ee892a96389f6ea
Shubham Kushwaha <103564639+Shubham36842@users.noreply.github.com>
2024-05-15 10:26:10 +0000

    new

--- Online_payment_fraud_detection/Online_payment_fraud_detection/views.py
+++ Online_payment_fraud_detection/Online_payment_fraud_detection/views.py
@@ -8,13 +8,13 @@ def homepage(request):
 		if request.method == "GET":
 			step = request.GET.get('step')    
 			txn  = request.GET.get('transaction')
-			amt  = int(request.GET.get('amount'))
+			amt  = float(request.GET.get('amount'))
 			cid = request.GET.get('Customerid')
 			rid = request.GET.get('Recipientid')
-			nb = int(request.GET.get('New Balance'))
-			ob = int(request.GET.get('Old Balance'))
-			rnb = int(request.GET.get('Recipient New Balance'))
-			rob = int(request.GET.get('Recipient Old Balance'))
+			nb = float(request.GET.get('New Balance'))
+			ob = float(request.GET.get('Old Balance'))
+			rnb = float(request.GET.get('Recipient New Balance'))
+			rob = float(request.GET.get('Recipient Old Balance'))
 			cash_out=debit=payment=transfer=0
 			if txn=='CASH_OUT':
 				cash_out=1
