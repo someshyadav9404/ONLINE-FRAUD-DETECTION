11db1c7f941bc57e6c978860e706c65133516741
someshyadav9404 <143208100+someshyadav9404@users.noreply.github.com>
2024-05-27 01:27:33 +0530

    Update views.py

--- Online_payment_fraud_detection/Online_payment_fraud_detection/views.py
+++ Online_payment_fraud_detection/Online_payment_fraud_detection/views.py
@@ -1,5 +1,27 @@
 from django.http import HttpResponse,HttpResponseRedirect
 from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate , login , logout
+from django.contrib import messages
+
+def login_user(request):
+	if request.method == "POST":
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request, username =username ,password= password)
+		if user is not None:
+			login(request,user)
+			return redirect('index')
+		else:
+			messages.success(request,("Invalid username or password . Please try Again...                                   "))
+			return redirect('login')
+	else:
+		return render(request,'registration/login.html')
+	
+def logout_user(request):
+	logout(request)
+	messages.success(request,("You have been logged Out"))
+	return redirect('index')
+
 import pandas as pd
 import joblib
 models=joblib.load('Trained_model.save')
